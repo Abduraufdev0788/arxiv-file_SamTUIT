62c0d50cc528c28036fa80f59750db09457fd8d6
Abduraufdev0788 <turkeynumber063@gmail.com>
2026-01-15 23:24:15 +0500

    init

--- core/settings.py
+++ core/settings.py
@@ -148,3 +148,5 @@ EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
